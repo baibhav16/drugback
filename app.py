1bc495866bac02e0128f2f6737ae84896574444f
Baibhav Kumar <147192139+baibhav16@users.noreply.github.com>
2025-04-11 19:20:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,5 +25,8 @@ def analyze():
         "recommendations": get_recommendations(drugs)
     })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render sets the PORT variable
+    app.run(host="0.0.0.0", port=port)
